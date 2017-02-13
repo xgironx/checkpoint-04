717b2b0da47ae9c9097122a8fd67afe031190f3f
xgironx <xgironx@gmail.com>
2017-02-13 10:01:36 -0500

    most of these i just stubbed, q4 i got further, i put separate questions into separate files so non-interfering

---
+++ app_q3_friends_names_belle.rb
@@ -0,0 +1,20 @@
+              #################################################
+              # 201702130951L   EL LUNES   JAY
+              #################################################
+
+              #################################################
+                      # NOW ONTO THE checkpoint-04, q3
+              # https://github.com/ga-wdi-exercises/checkpoint-04
+
+              # Ruby Basics & Enumerables
+              Question 3
+
+              Assume you have an array of strings representing friend's names...
+
+              friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
+              Using .each and string interpolation, puts the following text...
+
+              Belle is friends with Chip Potts
+              Belle is friends with Cogsworth
+              Belle is friends with Lumière
+              Belle is friends with Mrs. Potts
